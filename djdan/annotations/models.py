e1f4d5a504f09fc1deff93bda555b2411049f7fc
Ben O'Steen <ben.osteen@bl.uk>
2013-09-02 15:49:00 +0000

    Updates

--- djdan/annotations/models.py
+++ djdan/annotations/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class AnnotationSession(models.Model):
+  startedby = models.CharField(max_length=30)
+  description = models.TextField() 
+  datestamp = models.DateTimeField(auto_now_add=True)
+
 class Annotation(models.Model):
   author = models.CharField(max_length=30)
   leftitem = models.CharField(max_length=255)
   rightitem = models.CharField(max_length=255)
-  session = models.ForeignKey(Session)
+  session = models.ForeignKey(AnnotationSession)
   annotation = models.TextField()
   datestamp = models.DateTimeField(auto_now_add=True)
 
-class Session(models.Model):
-  startedby = models.CharField(max_length=30)
-  description = models.TextField() 
-  datestamp = models.DateTimeField(auto_now_add=True)
--- djdan/settings.py
+++ djdan/settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'djdan',                      # Or path to database file if using sqlite3.
+        'USER': 'ben',                      # Not used with sqlite3.
+        'PASSWORD': 'annotationstore',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'djdan.annotations',
 )
 
 # A sample logging configuration. The only tangible logging
