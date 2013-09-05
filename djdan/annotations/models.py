db41e949c583d270a3e00fb3f4ed44bb35672c30
Ben O'Steen <ben.osteen@bl.uk>
2013-09-05 15:53:21 +0000

    Added the Collection and Item models, and reworked the annotation record to store more

--- djdan/annotations/models.py
+++ djdan/annotations/models.py
@@ -1,16 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Collection(models.Model):
+  creator = models.ForeignKey(User)
+  tag = models.CharField(max_length=100)
+  
+  def __unicode__(self):
+    return self.tag
+
+class Item(models.Model):
+  TYPES = (
+    ('img', 'Image'),
+    ('web', 'Webpage'),
+    ('txt', 'Text quotation'),
+    ('aud', 'Audio file'),
+    ('vid', 'YouTube video'),
+  )
+
+  creator = models.ForeignKey(User)
+  tag = models.CharField(max_length=100, blank=True)
+  itemtype = models.CharField(max_length=3,
+                              choices=TYPES,
+                              default='img')
+  payload = models.TextField()
+  src = models.TextField(blank=True)
+  collection = models.ForeignKey(Collection, related_name="playlist")
+
+  def __unicode__(self):
+    if self.tag:
+      return self.tag
+    elif (self.payload) > 10:
+      return self.payload[:10] + "..."
+    else:
+      return self.payload
 
 # Create your models here.
 class AnnotationSession(models.Model):
-  startedby = models.CharField(max_length=30)
-  description = models.TextField() 
+  creator = models.ForeignKey(User)
+  title = models.CharField(max_length=100, blank=True)
+  description = models.TextField(blank=True) 
   datestamp = models.DateTimeField(auto_now_add=True)
+  def __unicode__(self):
+    return self.title
 
 class Annotation(models.Model):
-  author = models.CharField(max_length=30)
-  leftitem = models.CharField(max_length=255)
-  rightitem = models.CharField(max_length=255)
-  session = models.ForeignKey(AnnotationSession)
-  annotation = models.TextField()
+  creator = models.ForeignKey(User)
+  leftitem = models.ForeignKey(Item, related_name="left")
+  rightitem = models.ForeignKey(Item, related_name="right")
+  session = models.ForeignKey(AnnotationSession, related_name="annotations")
+  annotation = models.TextField(blank=True)
   datestamp = models.DateTimeField(auto_now_add=True)
-
+  
+  def __unicode__(self):
+    return "%s <-> %s" % (self.leftitem.__unicode__(), self.rightitem.__unicode__())
