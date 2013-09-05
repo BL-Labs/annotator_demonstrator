50d37907f96c4b21f36fb952f92d51a6474e4d37
Ben O'Steen <ben.osteen@bl.uk>
2013-09-05 16:55:29 +0000

    Updated settings

--- djdan/settings.py
+++ djdan/settings.py
@@ -106,10 +106,17 @@ ROOT_URLCONF = 'djdan.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'djdan.wsgi.application'
 
+import os
+
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    DIRNAME+'/site-templates/',
+    
 )
 
 INSTALLED_APPS = (
@@ -120,7 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'djdan.annotations',
