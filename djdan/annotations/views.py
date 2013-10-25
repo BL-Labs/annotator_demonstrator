ea7cc5eb654889f2c4bba0855fa183fb916bdd9e
Ben O'Steen <bosteen@gmail.com>
2013-10-25 15:08:43 +0100

    Making logging out easier

--- djdan/annotations/views.py
+++ djdan/annotations/views.py
@@ -38,7 +38,7 @@ def login_view(request):
 
 def logout_view(request):
   logout(request)
-  redirect("login_view")  
+  redirect("loginview")  
 
 def index(request):
   template = loader.get_template("annotations/index.html")
